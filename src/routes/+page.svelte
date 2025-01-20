e6c3920276210f84fbcf6fe7e7fba0be55472f07
烈火 <rekka69@proton.me>
2025-01-20 23:15:55 +0200

    intial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { invoke } from "@tauri-apps/api/core";
+
+</script>
+
+<main class="container">
+  <h1>明日、クフタさんは同性間結婚してます</h1>
+</main>
+
+<style>
+  h1 {
+    background: var(--main-mauve);
+  } 
+</style>

0d28e9b7da114335a726fe4e4ccb87b33c7f6da8
kofta999 <mostafaxxx555@gmail.com>
2025-01-21 22:24:44 +0200

    feat: add description, error handling and typed responses

--- src-tauri/src/vndb.rs
+++ src-tauri/src/vndb.rs
@@ -1,5 +1,4 @@
-use serde::Serialize;
-use serde_json::Value;
+use serde::{Deserialize, Serialize};
 use tauri_plugin_http::reqwest;
 
 // POST Request
@@ -11,24 +10,59 @@ struct VndbRequest<'a> {
     fields: &'a str,
 }
 
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+struct VndbResponse {
+    more: bool,
+    results: Vec<VndbGame>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct VndbGame {
+    id: String,
+    title: String,
+    image: Image,
+    // String, possibly null, may contain formatting codes.
+    description: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Image {
+    url: String,
+}
+
 #[tauri::command]
-pub async fn fetch_vn_info(key: String) {
+// Do I even need to do the deserialize / serialize thing or return the json as-is?
+pub async fn fetch_vn_info(key: String) -> Result<Vec<VndbGame>, String> {
+    let error_message = String::from("Error happened while fetching game");
+
     let request_data = VndbRequest {
         filters: vec!["search", "=", key.as_str()],
-        fields: "title, image.url",
+        fields: "id, title, image.url, description",
     };
 
     // TODO: Add error handling
     let client = reqwest::Client::new();
-    let result = client
+    let response = client
         .post(VNDB_URL)
         .json(&request_data)
         .send()
         .await
-        .unwrap();
+        .map_err(|e| {
+            dbg!(&e);
+            // Idk if using clone is correct but, it should be dropped from stack anyway
+            // + function will return on only of of the errors
+            error_message.clone()
+        })?;
 
-    dbg!(&result);
-    // TODO: Make strong type for this
-    let json: Value = result.json().await.unwrap();
-    dbg!(&json);
+    if response.status() != 200 {
+        return Err(error_message);
+    }
+
+    let json: VndbResponse = response.json().await.map_err(|e| {
+        dbg!(&e);
+        error_message.clone()
+    })?;
+
+    Ok(json.results)
 }

cb6efaf96b4d3b620b80bf716e80e895a0d16999
烈火 <rekka69@proton.me>
2025-01-23 22:40:18 +0200

    Add custom window decorations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
   import '../app.css';
+  import { getCurrentWindow } from '@tauri-apps/api/window';
+
+  // when using `"withGlobalTauri": true`, you may use
+  // const { getCurrentWindow } = window.__TAURI__.window;
+
+  const appWindow = getCurrentWindow();
+
+  document.getElementById('titlebar-minimize')
+    ?.addEventListener('click', () => appWindow.minimize());
+  document.getElementById('titlebar-close')
+    ?.addEventListener('click', () => appWindow.close());
 </script>
 
 <slot />

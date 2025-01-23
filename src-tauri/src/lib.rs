0034147e7c91b8b180ccdf96c140475ef6920e62
kofta999 <mostafaxxx555@gmail.com>
2025-01-24 00:38:34 +0200

    feat: add basic command for game saving
    助けてください、俺はもう力がない。「WE」死ね！

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,11 @@
+use tauri::Manager;
+use tauri_plugin_fs::FsExt;
+use tauri_plugin_store::StoreExt;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+mod storage;
 mod vndb;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -8,9 +14,25 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, vndb::fetch_vn_info])
+        .setup(|app| {
+            app.fs_scope()
+                .allow_directory(
+                    app.path().app_local_data_dir().unwrap().join("/images"),
+                    true,
+                )
+                .unwrap();
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            vndb::fetch_vn_info,
+            storage::save_game
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
